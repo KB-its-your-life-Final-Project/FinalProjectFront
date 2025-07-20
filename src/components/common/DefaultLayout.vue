079a7586504e44badd0ae23f9367d471e5df4783
laeongmulti <laeongmulticampus@gmail.com>
2025-07-21 08:25:25 +0900

    FEAT: 구조 수정 1차

--- src/components/common/DefaultLayout.vue
+++ src/components/common/DefaultLayout.vue
@@ -1,37 +1,11 @@
-<script setup lang="ts">
-import Header from "./Header.vue";
-import Footer from "./Footer.vue";
-
-</script>
-
-<!--<template>
-  <div class="DefaultLayout">
-    <Header />
-    <slot></slot>
-    <Footer />
-
-  </div>
-</template>-->
+<script setup lang="ts"></script>
 
 <template>
-<!--  <div class="h-screen flex flex-col overflow-auto">-->
-  <div class="min-h-screen flex flex-col items-center bg-gray-50">
-
-<!--    <div class="w-full max-w-[430px] flex flex-col flex-1">-->
+  <div class="min-h-screen flex flex-col items-center">
     <div class="w-full flex flex-col flex-1">
-    <main class="flex-1 overflow-auto">
-      <slot></slot>
-    </main>
-
-
-    <Section />
-  </div>
+      <div class="flex-1 overflow-auto">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
-
-
-
-<style scoped>
-.DefaultLayout {
-}
-</style>
