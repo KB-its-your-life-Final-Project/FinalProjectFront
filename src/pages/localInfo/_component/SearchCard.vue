c477e7d15b04a47bf3514a4b2b4eeff333ab699e
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-07-22 12:18:48 +0900

    Feat: 로고 서치바 삽입

--- src/pages/localInfo/_component/SearchCard.vue
+++ src/pages/localInfo/_component/SearchCard.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import LogoSearchBar from "@/pages/home/_component/LogoSearchBar.vue";
+</script>
+
 <template>
   <div class="w-full flex items-center justify-start pl-6">
-    <span class="font-pretendard-extrabold text-5xl text-kb-ui-11">화양동 동네이야기</span>
+    <span class="font-pretendard-extrabold text-5xl text-kb-ui-11">화양동 </span>
+    <LogoSearchBar />
   </div>
 </template>
