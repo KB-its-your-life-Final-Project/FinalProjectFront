96ac77190a73156453cf32d6d887b4c91c48ba38
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-07-22 12:17:36 +0900

    Refactor: 폰트어썸 구조 변경

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "@/assets/styles/main.css";
-import '@/assets/styles/colors.css';
+import "@/assets/styles/colors.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
@@ -7,37 +7,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faHouse,
-  faBars,
-  faBell,
-  faMagnifyingGlass,
-  faStar as fasStar,
-  faArrowLeft,
-  faVideo,
-  faUsers,
-  faShop,
-  faUtensils,
-} from "@fortawesome/free-solid-svg-icons"; // 홈
-import { faMap, faStar as farStar, faCircleXmark } from "@fortawesome/free-regular-svg-icons"; // 지도, 관심, 전체메뉴
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(
-  faHouse,
-  faMap,
-  fasStar,
-  faBars,
-  faBell,
-  faMagnifyingGlass,
-  faCircleXmark,
-  farStar,
-  faArrowLeft,
-  faVideo,
-  faUsers,
-  faShop,
-  faUtensils,
-);
+import { FontAwesomeIcon } from "@/plugins/fontawesome";
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
