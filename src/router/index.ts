667ba4f0fb0c918fb9e3920adab76e9aacdf7926
dahyoon <w2crosss@gmail.com>
2025-07-24 13:24:47 +0900

    WIP: 현재 작업 내용 저장

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 import { authRouteRecordRaw } from "@/router/authRoute";
 import { myPageRouteRecordRaw } from "@/router/mypageRoutes";
 import { mainRouteRecordRaw } from "@/router/mainRoute";
+import { authStore } from "@/stores/authStore";
 
 const routes: RouteRecordRaw[] = [
   //메인 라우트
@@ -36,5 +37,24 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+// 로그인 인증 가드
+router.beforeEach(async (to, from, next) => {
+  if(!to.meta.requiresAuth) {
+    return next(); // 통과
+  }
+  try {
+    const auth = authStore();
+    await auth.checkLogin(); // access token 유효성 확인
+    next();
+  } catch (error) {
+    try {
+      await auth.refreshTokens(); // refreshToken 시도
+      next();
+    } catch (error) {
+      next("/auth/login"); // 로그인 화면 이동
+    }
+  }
+})
 
 export default router;
