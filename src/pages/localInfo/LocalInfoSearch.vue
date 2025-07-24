10f55e98b556384782fe6ba1bd8a5e6d4cc5bb5d
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-07-24 14:32:30 +0900

    Feat: 검색 페이지 생성

--- src/pages/localInfo/LocalInfoSearch.vue
+++ src/pages/localInfo/LocalInfoSearch.vue
@@ -6,4 +6,15 @@ import { mainRouteName } from "@/router/mainRoute";
 </script>
 <template>
   <Header :headerShowtype="mainRouteName.localInfoSearch" />
+  <div class="w-full h-full flex-col px-4 mt-8">
+    <div class="font-pretendard-bold text-3xl text-kb-ui-01">지역을</div>
+    <div class="font-pretendard-bold text-3xl text-kb-ui-01">검색해주세요</div>
+    <div class="w-ful h-full mt-8">
+      <input
+        type="text"
+        placeholder="검색"
+        class="w-full rounded-xl border-2 border-kb-yellow bg-white shadow-sm px-4 py-2"
+      />
+    </div>
+  </div>
 </template>
