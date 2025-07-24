7f7e8f7895afca53ccf96b975d8820bfd1484505
cheonkio <heelun8525@naver.com>
2025-07-24 14:20:54 +0900

    feat: 마이페이지 컴포넌트 구현
    5가지 모달 컴포넌트
    사용자 집 입력 시에 사용되는 주소 검색 컴포넌트
    수정 Drawer안에 들어가는 item 컴포넌트
    사용자 집 정보 제공에 사용되는 InfoCard 컴포넌트
    
    Related to: #21

---
+++ src/components/mypage/ChangeNameModal.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ModalForm from "../common/ModalForm.vue";
+import CustomInput from "./CustomInput.vue";
+import { isEmpty } from "@/utils/validate";
+defineProps<{
+  oldName: string;
+}>();
+
+const emit = defineEmits(["close"]);
+const newName = ref("");
+function handleConfirm(): { success: boolean; message: string } {
+  if (isEmpty(newName.value)) {
+    return { success: false, message: "변경할 이름을 입력해 주세요" };
+  }
+  /** todo: 변경 요청 api 호출 */
+  return { success: true, message: "이름이 변경되었습니다" };
+}
+</script>
+<template>
+  <ModalForm :title="'이름 변경'" :handle-confirm="handleConfirm" @close="emit('close')">
+    <CustomInput
+      class="w-2/3"
+      :label="'기존 이름'"
+      :disabled="true"
+      :old-data="oldName"
+      :type="'text'"
+    ></CustomInput>
+    <CustomInput
+      class="mt-5 w-2/3"
+      :label="'변경 이름'"
+      v-model="newName"
+      type="'text'"
+    ></CustomInput>
+  </ModalForm>
+</template>
---
+++ src/components/mypage/InfoCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  content: string;
+  subContent: string;
+}>();
+</script>
+
+<template>
+  <div class="bg-white mx-4 mt-4 p-4 rounded-xl shadow-md">
+    <h3 class="text-sm font-semibold mb-1">{{ title }}</h3>
+    <p class="text-sm">{{ content }}</p>
+    <p class="text-xs text-gray-500">{{ subContent }}</p>
+  </div>
+</template>
