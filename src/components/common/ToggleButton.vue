ef7d2e63b35fbeb456b3732bbd2843af02e667ec
cheonkio <heelun8525@naver.com>
2025-07-25 14:20:16 +0900

    feat: 알림 설정 화면 구성 및 토글 버튼 컴포넌트 생성
    알림 설정 페이지 생성하여 라우터까지 설정
    설정 페이지에서 사용할 토글 버튼 공용 컴포넌트로 생성

---
+++ src/components/common/ToggleButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: boolean): void;
+}>();
+
+const isOn = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: boolean) {
+    emit("update:modelValue", value);
+  },
+});
+
+function toggle() {
+  isOn.value = !isOn.value; // computed의 setter 호출 -> emit 발생
+}
+</script>
+
+<template>
+  <button
+    :class="[
+      'relative w-15 h-7 rounded-full transition-colors duration-300',
+      isOn ? 'bg-kb-yellow' : 'bg-gray-200',
+    ]"
+    @click="toggle"
+  >
+    <div
+      class="w-6 h-6 bg-white rounded-full shadow-md absolute top-1/2 transform -translate-y-1/2 transition-transform duration-300"
+      :class="isOn ? 'translate-x-8' : 'translate-x-1'"
+    ></div>
+  </button>
+</template>
