469a2e3991067bf749b225eb242fc9ff72c3161a
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-07-25 13:27:00 +0900

    Refactor: 글자 크기 변경

--- src/pages/localInfo/_component/WeatherCard.vue
+++ src/pages/localInfo/_component/WeatherCard.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex items-center rounded-2xl border-1 border-blue-200 bg-white shadow-sm w-full">
-    <!-- 온도 정보 -->
-    <div class="flex flex-col items-center w-full mt-8">
-      <div class="font-pretendard-bold text-6xl text-gray-800 mb-2">34°</div>
-      <div class="text-lg font-pretendard-regular mb-2">
-        <div class="font-pretendard-bold text-kb-ui-04">최고 36°</div>
-        <div class="font-pretendard-bold text-kb-ui-04">최저 26°</div>
+  <div class="rounded-2xl border-1 border-blue-200 bg-white shadow-sm w-full">
+    <div class="flex items-center justify-between mt-3">
+      <!-- 온도 정보 -->
+      <div class="flex flex-col items-center w-full">
+        <div class="font-pretendard-bold text-4xl text-gray-800">34°</div>
+        <div class="font-pretendard-bold text-sm text-kb-ui-04">최고 36°</div>
+        <div class="font-pretendard-bold text-sm text-kb-ui-04">최저 26°</div>
       </div>
-    </div>
-    <div class="flex flex-col items-center w-full">
-      <div class="text-7xl mb-2">🌤️</div>
-      <div class="text-xs text-kb-ui-05 mt-5">출처 : 기상청</div>
+      <!-- 날씨 이미지 -->
+      <div class="flex flex-col items-center w-full gap-3">
+        <div class="text-4xl">🌤️</div>
+        <div class="text-xs text-kb-ui-05">출처 : 기상청</div>
+      </div>
     </div>
   </div>
 </template>
