ac2962c192ee971b8c38d64b24683044025872e6
laeongmulti <laeongmulti@gmail.com>
2025-08-08 16:20:43 +0900

    Fix: 지도 및 홈 레이아웃 수정 & 지도 에서 실거래가 데이터 수정

--- src/pages/home/HomeMain.vue
+++ src/pages/home/HomeMain.vue
@@ -17,16 +17,18 @@ import { mainRouteName } from "@/router/mainRoute";
   <div class="min-h-screen flex flex-col items-center bg-kb-ui-11">
     <div class="w-full flex flex-col flex-1">
       <Header :headerShowtype="mainRouteName.homeMain">
-        <div class="pl-8 pr-8 pt-14 pb-10">
-          <h2 class="text-sm font-bold text-kb-ui-01 font-semibold">
-            원룸, 빌라, 오피스텔, 아파트
-          </h2>
-          <h1 class="text-lg font-bold mt-[0.3rem]">집에 대한 모든 정보를 찾아보세요!</h1>
+        <div class="h-25 flex flex-col justify-center p-2">
+          <div class="">
+            <h2 class="text-sm font-bold text-kb-ui-01 font-semibold">
+              원룸, 빌라, 오피스텔, 아파트
+            </h2>
+            <h1 class="text-lg font-bold mt-[0.3rem]">집에 대한 모든 정보를 찾아보세요!</h1>
+          </div>
         </div>
 
-        <div class="px-4 mt-3">
-          <LogoSearchBar />
-        </div>
+        <!-- <div> -->
+        <LogoSearchBar />
+        <!-- </div> -->
       </Header>
 
       <main class="flex-1 px-4 pt-4 space-y-4 overflow-auto pb-24">
